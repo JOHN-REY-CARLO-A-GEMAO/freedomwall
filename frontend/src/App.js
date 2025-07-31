037193129a69fec2d75d94ef37aa1b97494ae85d
JOHN REY CARLO A. GEMAO <143941849+JOHN-REY-CARLO-A-GEMAO@users.noreply.github.com>
2025-07-31 12:58:59 +0800

    add-auth-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>

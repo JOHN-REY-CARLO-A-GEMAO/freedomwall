f18ed4ad9bd5354fdeb3733566978b140b633d17
im-jerz <doronilajerson6@gmail.com>
2025-07-31 19:38:15 +0800

    Revert "add-auth-ui"
    This reverts commit 037193129a69fec2d75d94ef37aa1b97494ae85d.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
-import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>

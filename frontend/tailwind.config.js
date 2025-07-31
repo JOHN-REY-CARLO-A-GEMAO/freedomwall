34b88c3983d3eedc3069812a666ef64fab4f3a40
im-jerz <doronilajerson6@gmail.com>
2025-07-31 11:07:54 +0800

    First Commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#dbeafe",
+          500: "#3b82f6",
+          900: "#1e3a8a",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
